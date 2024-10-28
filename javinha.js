30769a6c03575058d4b796571b46cb9a678a342d
DonavioNovelh <donavioev@gmail.com>
2024-10-28 15:03:03 +0200

    splsa

---
+++ javinha.js
@@ -0,0 +1,31 @@
+var input = document.querySelector('.ema');
+var form =  document.querySelector('.bt');
+var dr =  document.querySelector('.but');
+var body =  document.querySelector('article');
+console.log(input);
+console.log(form);
+input.value = 'jdjasha@hjajsa';
+
+form.addEventListener('submit', (eventt) => {
+    
+    eventt.preventDefault();
+    console.log(input.checkValidity());
+
+    body.setAttribute('class',"articlevairSumir");
+    window.location = 'sucess.html'
+    
+  
+})
+
+dr.addEventListener('click', (ee) => {
+    if(input.typeMismatch) {
+        message = 'the thins'
+        input.setCustomValidity(message);
+      
+        console.log('Email válido:', input.value);
+
+        
+    }
+})
+
+
